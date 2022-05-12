235070aa9bece196fdf010b627c1829eca67fd32
Siyuan Peng <peng2000@umd.edu>
2022-05-12 15:34:10 -0400

    add early stop

--- gradient_descent.py
+++ gradient_descent.py
@@ -3,49 +3,68 @@ Gradient Descent Method Implementation
 '''
 import numpy as np
 from sklearn.metrics import accuracy_score
-# define sigmoid function for gradient descent method
-def _sigmoid_function(self, x):
-    if x >= 0:
-        z = np.exp(-x)
-        return 1 / (1 + z)
-    else:
-        z = np.exp(x)
-        return z / (1 + z)
 
-def compute_loss(self, y_true, y_pred):
-    # binary cross entropy
-    y_zero_loss = y_true * np.log(y_pred + 1e-9)
-    y_one_loss = (1-y_true) * np.log(1 - y_pred + 1e-9)
-    return -np.mean(y_zero_loss + y_one_loss)
+class LogisticRegression():
+    def __init__(self, learning_rate):
+        self.losses = []
+        self.train_accuracies = []
+        self.l_rate = learning_rate
 
-def compute_gradients(self, x, y_true, y_pred):
-    # derivative of binary cross entropy
-    difference =  y_pred - y_true
-    gradient_b = np.mean(difference)
-    gradients_w = np.matmul(x.transpose(), difference)
-    gradients_w = np.array([np.mean(grad) for grad in gradients_w])
+    # define sigmoid function for gradient descent method
+    def sigmoid(self, x):
+        return 1/(1 + np.exp(-x))
 
-    return gradients_w, gradient_b
+    def compute_loss(self, y_true, y_pred):
+        # binary cross entropy
+        y_zero_loss = y_true * np.log(y_pred + 1e-9)
+        y_one_loss = (1-y_true) * np.log(1 - y_pred + 1e-9)
+        return -np.mean(y_zero_loss + y_one_loss)
 
-def update_model_parameters(self, error_w, error_b):
-    self.weights = self.weights - 0.1 * error_w
-    self.bias = self.bias - 0.1 * error_b
+    def compute_gradients(self, x, y_true, y_pred):
+        # derivative of binary cross entropy
+        difference =  y_pred - y_true
+        gradient_b = np.mean(difference)
+        gradients_w = np.matmul(x.transpose(), difference)
+        gradients_w = np.array([np.mean(grad) for grad in gradients_w])
 
-def fit(self, x, y, epochs):
-    x = self._transform_x(x)
-    y = self._transform_y(y)
+        return gradients_w, gradient_b
 
-    self.weights = np.zeros(x.shape[1])
-    self.bias = 0
+    def update_model_parameters(self, error_w, error_b):
+        self.weights = self.weights - self.l_rate * error_w
+        self.bias = self.bias - self.l_rate * error_b
 
-    for i in range(epochs):
-        x_dot_weights = np.matmul(self.weights, x.transpose()) + self.bias
-        pred = self._sigmoid(x_dot_weights)
-        loss = self.compute_loss(y, pred)
-        error_w, error_b = self.compute_gradients(x, y, pred)
-        self.update_model_parameters(error_w, error_b)
+    def fit(self, x, y, epochs, verbose):
+        self.weights = np.zeros(x.shape[1])
+        self.bias = 0
+        early_stop_tol = 1e-5
+        early_stop_buffer = []
 
+        for i in range(epochs):
+            output = np.matmul(self.weights, x.transpose()) + self.bias
+            pred = self.sigmoid(output)
+            loss = self.compute_loss(y, pred)
+            error_w, error_b = self.compute_gradients(x, y, pred)
+            self.update_model_parameters(error_w, error_b)
+
+            pred_to_class = [1 if p > 0.5 else 0 for p in pred]
+            acc = accuracy_score(y, pred_to_class)
+            self.train_accuracies.append(acc)
+            self.losses.append(loss)
+
+            if verbose: print(f'Epoch {i}: loss:{loss}; acc:{acc}\n')
+            # implement early stop
+            if len(early_stop_buffer) > 5:
+                early_stop_buffer.pop(0)
+            early_stop_buffer.append(acc)
+            if len(early_stop_buffer) == 5 and (max(early_stop_buffer) - min(early_stop_buffer) < early_stop_tol):
+                break # exit for loop
+        return [self.weights, self.bias], [self.train_accuracies, self.losses]
+    
+    def test(self, test_x, test_y):
+        output = np.matmul(self.weights, test_x.transpose()) + self.bias
+        pred = self.sigmoid(output)
         pred_to_class = [1 if p > 0.5 else 0 for p in pred]
-        self.train_accuracies.append(accuracy_score(y, pred_to_class))
-        self.losses.append(loss)
+        test_acc = accuracy_score(test_y, pred_to_class)
+        return test_acc
+
 
